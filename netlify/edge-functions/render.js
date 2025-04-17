19f0d92b37a68dbe22e844fbd70a0488c7fcfe8f
projecthypothesis <xanderkhamidov@gmail.com>
2025-04-18 03:07:13 +0500

    Fix Netlify build error with adapter configuration

--- netlify/edge-functions/render.js
+++
@@ -1,3 +0,0 @@
-// Эта функция нужна для корректной работы SvelteKit с Netlify Edge Functions
-// SvelteKit автоматически заполнит этот файл при сборке проекта
-export { handler } from '@sveltejs/adapter-netlify'; 
